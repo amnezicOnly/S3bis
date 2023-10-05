dfe252f3855de9d5257eddd6b639a35799ee12e9
Antoine Lévêque <antoine.leveque@epita.fr>
2023-10-05 19:24:15 +0200

    fin dm vraiment

--- Algo/DM1/test_dm.py
+++ Algo/DM1/test_dm.py
@@ -19,5 +19,5 @@ def trouver_mots_manquants_bis(liste1, liste2):
     mots_manquants = [mot for mot in liste1 if mot not in liste2]
     return mots_manquants
 
-arbre = buildtree("/home/amnezic/Desktop/S3bis/Algo/DM1/output.txt")
+arbre = buildtree("/home/amnezic/Desktop/S3bis/Algo/DM1/lexicons/liste.txt")
 print(len(wordlist(arbre)))
