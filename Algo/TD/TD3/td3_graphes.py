689bf03a44b377e879697c4a6f7d8fa90b3f0d09
Antoine Lévêque <antoine.leveque@epita.fr>
2023-11-07 12:51:56 +0100

    Ex 1.3 et 1.4 faits

--- Algo/TD/TD3/td3_graphes.py
+++ Algo/TD/TD3/td3_graphes.py
@@ -9,3 +9,39 @@ Graph :
     - Graph.directed : orienté ou non
     - Graph.adjlist[x] : liste d'adjacence du sommet x (succeseurs de x)
 """
+
+from algo_py import graphmat,graph,queue
+
+def degrees(Gmat):
+    res = []
+    for i in range(Gmat.order):
+        res.append(len(Gmat.adj[i]))
+    return res
+
+def in_out_degrees(G):
+    in_degrees = [0]*G.order
+    out_degrees = []
+    for i in range(G.order):
+        out_degrees.append(len(G.adjlist[i]))
+        for elt in G.adjlist[i]:
+            in_degrees[elt]+=1
+    return (in_degrees,out_degrees)
+
+def dot(G):
+    s = ""
+    if G.oriented:
+        s += "digraph {\n"
+        for i in range(G.order):
+            if len(G.adjlist(i))!=0:
+                for elt in G.adjlist:
+                    s += str(i) + " -> " + str(elt) + "\n"
+        s += "}"
+    else:
+        s += "graph {\n"
+        for i in range(G.order):
+            if len(G.adjlist(i))!=0:
+                for elt in G.adjlist:
+                    if elt>i:
+                        s += str(i) + " -- " + str(elt) + "\n"
+        s += "}"
+    return s
